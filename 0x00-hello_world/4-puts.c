04cf32e2f6be96c4d618db63a25488ea6e4e879f
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-08 07:49:40 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
+	char test_str[] = "\"Programming is like building a multilingual puzzle";
 
-char test_str[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-puts(test_str);
-
-return (0);
-
+	puts(test_str);
+	return (0);
 }
