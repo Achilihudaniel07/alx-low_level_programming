48ad9400c723dd0fcb62b5a84eb7ccbfb0192975
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-10 00:19:34 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,55 +1,26 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
+	char c = 'a';
+	char C = 'A';
 
-char c = a;
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
 
-char C = A;
-
-
-
-while (c <= z)
-
-{
-
-putchar(c);
-
-c++;
-
+	return (0);
 }
-
-while (C <= Z)
-
-{
-
-putchar(C);
-
-C++;
-
-}
-
-putchar(n);
-
-
-
-return (0);
-
-}
-=======
-
->>>>>>> f368f56858b06225941efc84221997b97b33df7c
