90672d7f65985679d8f36e3d9380799a7dbf579d
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-09 23:38:36 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,13 +8,13 @@
 
 /**
  *
- *  * main - Entry point
+ *   main - Entry point
  *
- *   *
+ *   
  *
- *    * Return: Always 0 (Success)
+ *     Return: Always 0 (Success)
  *
- *     */
+ */
 
 int main(void)
 
