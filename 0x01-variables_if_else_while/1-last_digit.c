cc2c2e9db2b990394133e53c97d89a491908d0d1
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-09 23:59:00 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,48 +1,32 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <time.h>
-
 #include <stdio.h>
 
-
-
 /**
+ * main - print if the number is positive, zero, or negative
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
-
 {
+	int n;
 
-		int n, last_d;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					last_d = n % 10;
-
-						if (last_d > 5)
-
-									printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
-
-							else if (last_d == 0)
-
-										printf("Last digit of %d is %d and is 0\n", n, last_d);
-
-								else
-
-											printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
-
-									return (0);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	  /* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
 }
-
