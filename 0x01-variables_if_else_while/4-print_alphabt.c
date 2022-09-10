9509e38fc7e013c533c3b103749e857029072d75
Achilihudaniel07 <achilihudaniel53@gmail.com>
2022-09-10 03:15:31 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+char c = a;
+
+
+
+while (c <= z)
+
+{
+
+if (c != q && c != e)
+
+putchar(c);
+
+
+
+c++;
+
+}
+
+putchar(n);
+
+
+
+return (0);
+
+}
