b49e6ca4bf2cf70cc2162a57d2b6cee33224eebf
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-10 11:24:59 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-         int n = 0;
-         
-         while (n < 10)
-         {
-                 printf("%d", n);
-                 n++;
-          }
-          putchar('\n');
-          
-          return (0);
+ int n = 0;
+
+ while (n < 10)
+ {
+  printf("%d", n);
+  n++;
+ }
+ putchar('\n');
+
+ return (0);
 }
