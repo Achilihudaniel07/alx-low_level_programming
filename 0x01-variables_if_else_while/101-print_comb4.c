061464ebdd2d5d9bcd9c5e50e442f25a9c792c2e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-10 12:14:10 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,83 +1,40 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
+	int d = 48, a = 48, i = 48;
 
-int d = 48, a = 48, i = 48;
+	while (d < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			i = 48;
+			while (i < 58)
+			{
+				if (d != a && a != i && d != i && d < a && a < i)
+				{
+					putchar(d);
+					putchar(a);
+					putchar(i);
+					if (!(d == 55 && a == 56 && i == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				i++;
+			}
+			a++;
+		}
+		d++;
+	}
+	putchar('\n');
 
-
-
-while (d < 58)
-
-{
-
-a = 48;
-
-while (a < 58)
-
-{
-
-i = 48;
-
-while (i < 58)
-
-{
-
-if (d != a && a != i && d != i && d < a && a < i)
-
-{
-
-putchar(d);
-
-putchar(a);
-
-putchar(i);
-
-if (!(d == 55 && a == 56 && i == 57))
-
-{
-
-putchar(,);
-
-putchar( );
-
+	return (0);
 }
-
-}
-
-i++;
-
-}
-
-a++;
-
-}
-
-d++;
-
-}
-
-putchar(n);
-
-
-
-return (0);
-
-}
-=======
-
->>>>>>> 6f355326dd52a71268efcd399980a4320791f1f6
