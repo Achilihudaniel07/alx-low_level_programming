447fdee79e932a595d8d5d13e9976f0911d84a1e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-13 21:24:11 +0100

    task0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
-
 *main - prints the word Holberton
-
 *
-
 *Return: 0 on success execution.
-
 */
 
-
-
 int main(void)
-
 {
+	char s[] = "_putchar";
+	int i = 0;
 
-char s[] = "_putchar";
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
 
-int i = 0;
-
-
-
-while (s[i] != 0)
-
-{
-
-_putchar(s[i]);
-
-i++;
-
-}
-
-_putchar(10);
-
-
-
-return (0);
-
+	return (0);
 }
