46213a6cba02d224a2427a2b5ec809bcb55b6f41
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-13 22:19:02 +0100

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,43 +1,26 @@
 #include "main.h"
 
-
-
 /**
+ * print_sign - prints the sign of a number
+ * @n: positive, zero or negative integer
+ * Return: 1 if it's positive, 0 if it's negative and -1 if it's negative
+ */
 
-*main - prints the word Holberton
-
-*
-
-*Return: 0 on success execution.
-
-*/
-
-
-
-int main(void)
-
+int print_sign(int n)
 {
-
-char s[] = "_putchar";
-
-int i = 0;
-
-
-
-while (s[i] != 0)
-
-{
-
-_putchar(s[i]);
-
-i++;
-
-}
-
-_putchar(10);
-
-
-
-return (0);
-
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
