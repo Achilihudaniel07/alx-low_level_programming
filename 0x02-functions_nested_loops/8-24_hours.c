64ae7e6884901ec4cf2f2da92adfd51f124e36c8
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-13 22:19:38 +0100

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,43 +1,41 @@
 #include "main.h"
 
-
-
 /**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
 
-*main - prints the word Holberton
-
-*
-
-*Return: 0 on success execution.
-
-*/
-
-
-
-int main(void)
-
+void jack_bauer(void)
 {
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-char s[] = "_putchar";
+	while (x < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
 
-int i = 0;
-
-
-
-while (s[i] != 0)
-
-{
-
-_putchar(s[i]);
-
-i++;
-
-}
-
-_putchar(10);
-
-
-
-return (0);
-
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
+	}
 }
