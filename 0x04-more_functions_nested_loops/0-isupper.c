197fd6e32c419a1709dbf74d539d75d23aff69c1
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-15 23:31:24 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - function that checks for uppercase character.
- * @c: character to be tested
+ * @c: int type number
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
+ 
 int _isupper(int c)
 {
 	if ((c >= 'A' ) && (c <= 'Z'))
