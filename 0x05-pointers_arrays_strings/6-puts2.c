68e901aecbb0543b1123fbe88119cf8a39b845fa
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-19 21:29:11 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,20 @@
-int _strlen(char *s)
+#include "main.h"
 
+/**
+ * puts2 - prints every other character of a given string
+ * @str: passed string pointer
+ *
+ * Return: void
+ */
+void puts2(char *str)
 {
+	int i = 0;
 
-0-reset_to_98.c int len = 0;
-
-
-
-0-reset_to_98.c while (*(s + len) != 0)
-
-0-reset_to_98.c 0-reset_to_98.c len++;
-
-
-
-0-reset_to_98.c return (len);
-
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
 }
