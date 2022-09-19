65469147584724035e530305b23b96e580c417c0
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-19 21:30:27 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,23 @@
-int _strlen(char *s)
+#include <stdio.h>
 
+/**
+ * print_array - prints first n elements of array
+ * @a: int array to display from
+ * @n: number of elements to display
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
 {
+	int i = 0;
 
-0-reset_to_98.c int len = 0;
-
-
-
-0-reset_to_98.c while (*(s + len) != 0)
-
-0-reset_to_98.c 0-reset_to_98.c len++;
-
-
-
-0-reset_to_98.c return (len);
-
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+		i++;
+	}
+	printf("\n");
 }
