6c361d40c364d2485878d8e54b50c8357c776346
Achilihudaniel07 <achilihudaniel53@gmail.com>
2022-09-19 13:20:01 -0700

    creating files

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+int _strlen(char *s)
+
+{
+
+0-reset_to_98.c int len = 0;
+
+
+
+0-reset_to_98.c while (*(s + len) != 0)
+
+0-reset_to_98.c 0-reset_to_98.c len++;
+
+
+
+0-reset_to_98.c return (len);
+
+}
