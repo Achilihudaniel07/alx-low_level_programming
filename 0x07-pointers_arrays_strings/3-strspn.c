56e3e0ebd608cd951c4f33a62630663b9f6d4667
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-27 00:49:00 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,30 @@
 #include "main.h"
 
-
-
 /**
-
- * _memset - fills memory with a constant byte
-
+ * _strspn - gets the length of a prefix substring
  *
-
- * @s: pointer to string
-
- * @b: constant byte
-
- * @n: first bytes to change
-
- * Return: pointer to modified string
-
+ * @s: string to work on
+ * @accept: pattern to match against
+ * Return: unsigned int to how many initial bytes match the pattern
  */
-
-char *_memset(char *s, char b, unsigned int n)
-
+unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int i, j, len = 0, match = 0;
 
-unsigned int i;
-
-
-
-for (i = 0; i < n; i++)
-
-s[i] = b;
-
-return (s);
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (len != 0)
+			match++;
+		if (match != len)
+			return (len);
+	}
+	return (0);
 }
