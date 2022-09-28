ad5d964097e81aa263f8a73100f72fc71b113ce1
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-28 11:53:45 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,51 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ *
+ * @s: string
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s), i = 0;
 
+	if (len == 0)
+		return (1);
+	return (palindrome_check(s, len - 1, i));
+}
 
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
 
-#include <stdio.h>
+	if (*s == '\0')
+		return (0);
+	len = _strlen_recursion(s + 1);
+	return (len + 1);
+}
 
-
-
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_sqrt(int n, int square);
-
-int is_prime_number(int n);
-
-int is_prime(int n, int i);
-
-int is_palindrome(char *s);
-
-int palindrome_check(char *s, int len, int i);
-
-int wildcmp(char *s1, char *s2);
-
-
-
-#endif /* MAIN_H */
+/**
+ * palindrome_check - returns 1 if palindrome and 0 if not
+ *
+ * @s: string
+ * @len: length of string
+ * @i: counter variable
+ * Return: int
+ */
+int palindrome_check(char *s, int len, int i)
+{
+	if (len == i)
+		return (1);
+	else if (s[len] != s[i])
+		return (0);
+	if (i < len)
+		return (palindrome_check(s, --len, ++i));
+	return (1);
+}
