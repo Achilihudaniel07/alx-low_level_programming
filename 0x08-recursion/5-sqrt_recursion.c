c1bda06697c7bdc9c0b8f37dfcd3c647ff8a6da8
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-28 11:55:55 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,36 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int square = 2;
 
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	return (is_sqrt(n, square));
+}
 
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_sqrt(int n, int square);
-
-int is_prime_number(int n);
-
-int is_prime(int n, int i);
-
-int is_palindrome(char *s);
-
-int palindrome_check(char *s, int len, int i);
-
-int wildcmp(char *s1, char *s2);
-
-
-
-#endif /* MAIN_H */
+/**
+ * is_sqrt - function to check whether it's a natural square root or not
+ *
+ * @n: number
+ * @square: test number
+ * Return: int
+ */
+int is_sqrt(int n, int square)
+{
+	if (square * square == n)
+		return (square);
+	else if (square * square < n)
+		return (is_sqrt(n, square + 1));
+	else if (square * square > n)
+		return (-1);
+	return (-1);
+}
