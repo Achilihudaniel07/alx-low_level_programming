7608a79f367429a25f79211cdcc0c8b6c869e2d8
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-28 11:54:35 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,39 +1,17 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
 
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_sqrt(int n, int square);
-
-int is_prime_number(int n);
-
-int is_prime(int n, int i);
-
-int is_palindrome(char *s);
-
-int palindrome_check(char *s, int len, int i);
-
-int wildcmp(char *s1, char *s2);
-
-
-
-#endif /* MAIN_H */
+	if (*s == '\0')
+		return (0);
+	len = _strlen_recursion(s + 1);
+	return (len + 1);
+}
