135cecc642a24f2de35097f51db3d3a39aad7cc1
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-28 11:55:05 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,39 +1,16 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_sqrt(int n, int square);
-
-int is_prime_number(int n);
-
-int is_prime(int n, int i);
-
-int is_palindrome(char *s);
-
-int palindrome_check(char *s, int len, int i);
-
-int wildcmp(char *s1, char *s2);
-
-
-
-#endif /* MAIN_H */
+/**
+ * factorial - returns the factorial of a given number or -1 if n is negative
+ *
+ * @n: number to factorize
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
