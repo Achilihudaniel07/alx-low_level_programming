f78fde1aeb47ab4f629b8b18c4cf4edf3508e531
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-09-28 11:55:32 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,39 +1,17 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
-void _print_rev_recursion(char *s);
-
-int _strlen_recursion(char *s);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_sqrt(int n, int square);
-
-int is_prime_number(int n);
-
-int is_prime(int n, int i);
-
-int is_palindrome(char *s);
-
-int palindrome_check(char *s, int len, int i);
-
-int wildcmp(char *s1, char *s2);
-
-
-
-#endif /* MAIN_H */
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: number to be raised
+ * @y: power
+ * Return: int of x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
