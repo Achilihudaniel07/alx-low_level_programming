af1d818d3fdf6521966258e08923f8a9a841ddde
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-03 15:34:42 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,43 @@
-#ifndef MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
-#define MAIN_H
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * Return: char pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0, j, size = 0;
+	char *str;
 
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void *malloc(size_t size);
-
-void free(void *ptr);
-
-char *create_array(unsigned int size, char c);
-
-char *_strdup(char *str);
-
-char *str_concat(char *s1, char *s2);
-
-int **alloc_grid(int width, int height);
-
-void free_grid(int **grid, int height);
-
-
-
-#endif /* MAIN_H */
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			size++;
+	if (s2 != NULL)
+		for (i = 0; s2[i] != '\0'; i++)
+			size++;
+	str = malloc(sizeof(char) * (size + 1));
+	if (str == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+	{
+		str[0] = '\0';
+		return (str);
+	}
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			str[i] = s1[i];
+	if (s1 == NULL)
+		i = 0;
+	if (s2 != NULL)
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			str[i] = s2[j];
+			i++;
+		}
+	str[size] = '\0';
+	return (str);
+}
