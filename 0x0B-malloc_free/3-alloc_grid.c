50b64fd87124ffaa1b235e41855426e89ad92e49
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-03 15:35:12 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,29 +1,40 @@
-#ifndef MAIN_H
+#include <stdlib.h>
 
-#define MAIN_H
+/**
+ * **alloc_grid - dynamically create 2d array and initialize each element to 0
+ * @width: no. of columns
+ * @height: no. of rows
+ *
+ * Return: pointer to the 2d array or NULL if failure occurs
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int index, i, j;
 
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void *malloc(size_t size);
-
-void free(void *ptr);
-
-char *create_array(unsigned int size, char c);
-
-char *_strdup(char *str);
-
-char *str_concat(char *s1, char *s2);
-
-int **alloc_grid(int width, int height);
-
-void free_grid(int **grid, int height);
-
-
-
-#endif /* MAIN_H */
+	if (width <= 0 || height <= 0)
+		return (0);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == 0)
+	{
+		free(a);
+		return (0);
+	}
+	for (index = 0; index < height; index++)
+	{
+		*(a + index) = (int *)malloc(sizeof(int) * width);
+		if (*(a + index) == 0)
+		{
+			for (i = 0; i < index; i++)
+				free(*(a + i));
+			free(a);
+			return (0);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			*(*(a + i) + j) = 0;
+	}
+	return (a);
+}
