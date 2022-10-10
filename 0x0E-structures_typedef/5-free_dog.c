2b3d5cd305aebc4b59cef06fb4b0d51dd9e8366a
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-10 16:49:38 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,35 +1,18 @@
+#include <stdlib.h>
 #include "dog.h"
 
-#include <stdlib.h>
-
-
-
 /**
-
- * free_dog - frees memory held by passed struct
-
- *
-
- * @d: struct to free
-
- * Return: nothing
-
+ * free_dog - free memory
+ *@d: array
+ * Description: Free memory for struct
  */
 
 void free_dog(dog_t *d)
-
 {
-
-if (d != NULL)
-
-{
-
-free(d->name);
-
-free(d->owner);
-
-free(d);
-
-}
-
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
