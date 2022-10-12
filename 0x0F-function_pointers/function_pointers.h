9c97e91e65678bcc9340dce306da1e7b372ab5be
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-12 11:48:14 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,27 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdlib.h>
 
-#include "function_pointers.h"
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 
-
-/**
-
- * print_name - prints a name
-
- * @name: name to print
-
- * @f: pointer to the printing function
-
- */
-
-void print_name(char *name, void (*f)(char *))
-
-{
-
-if (!name || !f)
-
-return;
-
-f(name);
-
-}
+#endif
