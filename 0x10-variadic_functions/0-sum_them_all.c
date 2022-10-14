71974503cda5b5c87693f80216419148617f8f46
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-14 05:48:01 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters.
@@ -6,6 +7,7 @@
  *
  * Return: sum of its parameters.
  */
+ 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
