26f93b6ed599854077ad065efe0d0a6ca8ac92b9
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-14 05:33:44 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,51 +1,12 @@
-#include "variadic_functions.h"
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
+#include <stdarg.h>
+#include <stdio.h>
 
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-/**
-
- * sum_them_all - returns the sum of all its parameters.
-
- * @n: amount of the arguments.
-
- *
-
- * Return: sum of its parameters.
-
- */
-
-int sum_them_all(const unsigned int n, ...)
-
-{
-
-va_list valist;
-
-unsigned int i;
-
-int sum = 0;
-
-
-
-if (n == 0)
-
-return (0);
-
-
-
-va_start(valist, n);
-
-
-
-for (i = 0; i < n; i++)
-
-sum += va_arg(valist, int);
-
-
-
-va_end(valist);
-
-
-
-return (sum);
-
-}
+#endif
