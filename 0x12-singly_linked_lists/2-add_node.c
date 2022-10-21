2fcf225b1e7547057e70accc221b69763d4109d7
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-21 03:21:26 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,29 @@
 #include "lists.h"
-
 /**
-
- * list_len - returns then number of elements in a list.
-
- * @h: singly linked list.
-
- * Return: number of elements in the list.
-
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
-
-
-size_t list_len(const list_t *h)
-
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	size_t nchar;
 
-size_t nelem;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
+	new->str = strdup(str);
 
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-nelem = 0;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-while (h != NULL)
-
-{
-
-h = h->next;
-
-nelem++;
-
-}
-
-return (nelem);
-
+	return (*head);
 }
