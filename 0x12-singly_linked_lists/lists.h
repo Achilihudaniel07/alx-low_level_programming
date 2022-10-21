d28e6fc4b88d59c44da024a9b3abb017bb1f4e93
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-21 03:22:46 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,37 +1,30 @@
-#include "lists.h"
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-
- * list_len - returns then number of elements in a list.
-
- * @h: singly linked list.
-
- * Return: number of elements in the list.
-
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
-
-
-size_t list_len(const list_t *h)
-
+typedef struct list_s
 {
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-size_t nelem;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-
-
-nelem = 0;
-
-while (h != NULL)
-
-{
-
-h = h->next;
-
-nelem++;
-
-}
-
-return (nelem);
-
-}
+#endif
