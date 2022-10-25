58bc6d66d7c3a0bf02c34f0dbe8e740bafa1b033
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-25 04:14:33 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,39 +1,20 @@
 #include "lists.h"
 
-
-
 /**
-
- * print_listint - prints all the elements of a list.
-
+ * listint_len - returns the number of elements in
+ * a linked list.
  * @h: head of a list.
-
  *
-
  * Return: numbers of nodes.
-
  */
-
-size_t print_listint(const listint_t *h)
-
+size_t listint_len(const listint_t *h)
 {
+	size_t nnodes = 0;
 
-size_t nnodes = 0;
-
-
-
-while (h != NULL)
-
-{
-
-printf("%d\n", h->n);
-
-h = h->next;
-
-nnodes++;
-
-}
-
-return (nnodes);
-
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
