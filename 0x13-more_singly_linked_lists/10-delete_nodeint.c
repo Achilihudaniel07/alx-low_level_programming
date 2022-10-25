46997b01a9b4a68179ee9d9ec0703fd4251d0cc4
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-25 04:15:06 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,47 @@
 #include "lists.h"
 
-
-
 /**
-
- * print_listint - prints all the elements of a list.
-
- * @h: head of a list.
-
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
  *
-
- * Return: numbers of nodes.
-
+ * Return: 1 if it succeeded, -1 if it failed.
  */
-
-size_t print_listint(const listint_t *h)
-
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-size_t nnodes = 0;
+	prev = *head;
 
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
 
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
 
-while (h != NULL)
+	next = prev->next;
 
-{
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
 
-printf("%d\n", h->n);
-
-h = h->next;
-
-nnodes++;
-
-}
-
-return (nnodes);
-
+	return (1);
 }
