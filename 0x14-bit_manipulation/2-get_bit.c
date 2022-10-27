b026aa437b7ef41601b676e64254f29d688e453e
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-27 15:18:13 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,53 +1,27 @@
 #include "main.h"
 
-
-
 /**
-
  * get_bit - returns the value of a bit at a given
-
  * index.
-
  * @n: unsigned long int input.
-
  * @index: index of the bit.
-
  *
-
  * Return: value of the bit.
-
  */
-
 int get_bit(unsigned long int n, unsigned int index)
-
 {
+	unsigned int i;
 
-0-binary_to_uint.c unsigned int i;
+	if (n == 0 && index < 64)
+		return (0);
 
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
-
-0-binary_to_uint.c if (n == 0 && index < 64)
-
-0-binary_to_uint.c 0-binary_to_uint.c return (0);
-
-
-
-0-binary_to_uint.c for (i = 0; i <= 63; n >>= 1, i++)
-
-0-binary_to_uint.c {
-
-0-binary_to_uint.c 0-binary_to_uint.c if (index == i)
-
-0-binary_to_uint.c 0-binary_to_uint.c {
-
-0-binary_to_uint.c 0-binary_to_uint.c 0-binary_to_uint.c return (n & 1);
-
-0-binary_to_uint.c 0-binary_to_uint.c }
-
-0-binary_to_uint.c }
-
-
-
-0-binary_to_uint.c return (-1);
-
+	return (-1);
 }
