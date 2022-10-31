990281dcc958fd6b509671b5ee836d7e054acac6
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-31 23:42:47 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,75 +1,36 @@
 #include "main.h"
 
-
-
 /**
-
- * create_file - creates a file
-
+ * read_textfile - reads a text file and prints the letters
  * @filename: filename.
-
- * @text_content: content writed in the file.
-
+ * @letters: numbers of letters printed.
  *
-
- * Return: 1 if it success. -1 if it fails.
-
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
-int create_file(const char *filename, char *text_content)
-
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-int fd;
+	if (!filename)
+		return (0);
 
-int nletters;
+	fd = open(filename, O_RDONLY);
 
-int rwr;
+	if (fd == -1)
+		return (0);
 
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-if (!filename)
+	close(fd);
 
-return (-1);
+	free(buf);
 
-
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-
-
-if (fd == -1)
-
-return (-1);
-
-
-
-if (!text_content)
-
-text_content = "";
-
-
-
-for (nletters = 0; text_content[nletters]; nletters++)
-
-;
-
-
-
-rwr = write(fd, text_content, nletters);
-
-
-
-if (rwr == -1)
-
-return (-1);
-
-
-
-close(fd);
-
-
-
-return (1);
-
+	return (nwr);
 }
