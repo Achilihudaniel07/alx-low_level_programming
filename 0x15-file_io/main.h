2be65e0af8dfa1f466bbac5b5eeec52af276d6eb
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-10-31 23:45:16 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,75 +1,14 @@
-#include "main.h"
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-/**
-
- * create_file - creates a file
-
- * @filename: filename.
-
- * @text_content: content writed in the file.
-
- *
-
- * Return: 1 if it success. -1 if it fails.
-
- */
-
-int create_file(const char *filename, char *text_content)
-
-{
-
-int fd;
-
-int nletters;
-
-int rwr;
-
-
-
-if (!filename)
-
-return (-1);
-
-
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-
-
-if (fd == -1)
-
-return (-1);
-
-
-
-if (!text_content)
-
-text_content = "";
-
-
-
-for (nletters = 0; text_content[nletters]; nletters++)
-
-;
-
-
-
-rwr = write(fd, text_content, nletters);
-
-
-
-if (rwr == -1)
-
-return (-1);
-
-
-
-close(fd);
-
-
-
-return (1);
-
-}
+#endif
