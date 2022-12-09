b06c756c1f2db22f0605ffa206ac02c28a594bcf
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-09 04:04:24 +0100

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,57 +1,52 @@
 #include "lists.h"
 
-
-
 /**
-
- * sum_dlistint - returns the sum of all the data (n)
-
- * of a doubly linked list
-
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
  *
-
  * @head: head of the list
-
- * Return: sum of the data
-
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
-int sum_dlistint(dlistint_t *head)
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-int sum;
+	h1 = *head;
 
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
+	i = 0;
 
-sum = 0;
+	while (h1 != NULL)
+	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
 
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
 
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
+	}
 
-if (head != NULL)
-
-{
-
-while (head->prev != NULL)
-
-head = head->prev;
-
-
-
-while (head != NULL)
-
-{
-
-sum += head->n;
-
-head = head->next;
-
-}
-
-}
-
-
-
-return (sum);
-
+	return (-1);
 }
