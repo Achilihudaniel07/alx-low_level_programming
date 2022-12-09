a5b8c4ee51653c588c29cc79c45a6e883e653d98
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-09 04:03:45 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,57 +1,54 @@
 #include "lists.h"
 
-
-
 /**
-
- * sum_dlistint - returns the sum of all the data (n)
-
- * of a doubly linked list
-
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
-
- * @head: head of the list
-
- * Return: sum of the data
-
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
-
-int sum_dlistint(dlistint_t *head)
-
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-int sum;
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-
-
-sum = 0;
-
-
-
-if (head != NULL)
-
-{
-
-while (head->prev != NULL)
-
-head = head->prev;
-
-
-
-while (head != NULL)
-
-{
-
-sum += head->n;
-
-head = head->next;
-
-}
-
-}
-
-
-
-return (sum);
-
+	return (new);
 }
