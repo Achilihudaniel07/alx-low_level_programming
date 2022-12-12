69f24d4895fe3dfe3a9159ad230dd6d824aed9d7
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-12 13:40:28 +0100

    Update 3-islower.c

--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,27 +1,13 @@
 #include "main.h"
-
 /**
-
-*_isupper - function that verifies if a  character is uppercase or not
-
-*@c: tested character
-
-*Return: returns 1 if it is uppercase , 0 if not
-
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
 */
-
-
-
-int _isupper(int c)
-
+int _islower(int c)
 {
-
-if ((c >= A) && (c <= Z))
-
+if (c >= 'a' && c <= 'z')
 return (1);
 
-
-
 return (0);
-
 }
