119f5af401a0eeed15faef238080a46276452166
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-12 13:48:19 +0100

    Update _putchar.c

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,27 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
-
-*_isupper - function that verifies if a  character is uppercase or not
-
-*@c: tested character
-
-*Return: returns 1 if it is uppercase , 0 if not
-
-*/
-
-
-
-int _isupper(int c)
-
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-
-if ((c >= A) && (c <= Z))
-
-return (1);
-
-
-
-return (0);
-
+	return (write(1, &c, 1));
 }
