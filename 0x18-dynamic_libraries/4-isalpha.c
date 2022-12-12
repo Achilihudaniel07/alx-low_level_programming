add684c29553d26342f6c206403502a67ebe72ea
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-12 13:44:04 +0100

    Update 4-isalpha.c

--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,27 +1,13 @@
 #include "main.h"
-
 /**
-
-*_isupper - function that verifies if a  character is uppercase or not
-
-*@c: tested character
-
-*Return: returns 1 if it is uppercase , 0 if not
-
+*_isalpha - controls if a character is alphabetical
+*@c: character to be verified
+*Return: return 0 or 1
 */
-
-
-
-int _isupper(int c)
-
+int _isalpha(int c)
 {
-
-if ((c >= A) && (c <= Z))
-
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 return (1);
 
-
-
 return (0);
-
 }
