6f295cec67ab83f562ecd1aa52898f83051a25dd
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-12 13:49:39 +0100

    Update function.c

--- 0x18-dynamic_libraries/function.c
+++ 0x18-dynamic_libraries/function.c
@@ -1,27 +1,56 @@
 #include "main.h"
 
 /**
-
-*_isupper - function that verifies if a  character is uppercase or not
-
-*@c: tested character
-
-*Return: returns 1 if it is uppercase , 0 if not
-
-*/
-
-
-
-int _isupper(int c)
-
+ * add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+int add(int a, int b)
 {
+	return (a + b);
+}
 
-if ((c >= A) && (c <= Z))
+/**
+ * sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
 
-return (1);
+/**
+ * mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
+/**
+ * div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
 
-
-return (0);
-
+/**
+ * mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
 }
