1abad9cd5040c2bb99b7ddff034885121abb5754
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2022-12-12 13:39:19 +0100

    Update 2-strncpy.c

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -1,27 +1,25 @@
 #include "main.h"
-
 /**
-
-*_isupper - function that verifies if a  character is uppercase or not
-
-*@c: tested character
-
-*Return: returns 1 if it is uppercase , 0 if not
-
+*  _strncpy - C function that copies a string, including the
+*  terminating null byte, using at most an inputted number of bytes.
+*  If the length of the source string is less than the maximum byte number,
+*  the remainder of the destination string is filled with null bytes.
+*  Works identically to the standard library function `strncpy`.
+*@dest: buffer storing the string copy
+*@src:the source string
+*@n:max nummber of byte copied
+*Return: returns
 */
 
-
-
-int _isupper(int c)
-
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-if ((c >= A) && (c <= Z))
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-return (1);
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-
-
-return (0);
-
+	return (dest);
 }
